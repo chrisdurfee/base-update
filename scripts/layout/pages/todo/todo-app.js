274596a5615c0b1f9f049ef338667078b1515d40
tech_e <chris.durfee@dentalqore.com>
2024-03-26 00:53:27 -0600

    Refactor Pod component and add new methods

--- scripts/layout/pages/todo/todo-app.js
+++ scripts/layout/pages/todo/todo-app.js
@@ -1,13 +1,130 @@
 import { Button, Div, Form, H1, Input, Li, Ul } from '@base-framework/atoms';
-import { Data, Pod } from '@base-framework/base';
+import { Component, Data } from '@base-framework/base';
+
+/**
+ * Pods
+ *
+ * Pods are components that are created using a shorthand method.
+ *
+ * @author Chris Durfee
+ * @author Cayon Christiansen
+ */
+const getDefaultMethods = () => (
+{
+    created() {},
+    setData() {
+        return null;
+    },
+    onCreated() {
+        this.created();
+
+        if (this.data)
+        {
+            return;
+        }
+
+        const data = this.setData();
+        if (data)
+        {
+            this.data = data;
+        }
+    },
+
+    setStates() {},
+    setupStates () {
+        return this.setStates();
+    },
+
+    events() {
+        return [];
+    },
+    setupEvents() {
+        return this.events();
+    },
+
+    before() {},
+    beforeSetup() {
+        this.before();
+    },
+
+    after() {},
+    afterSetup() {
+        this.after();
+    },
+
+    destroy() {},
+    beforeDestroy() {
+        this.destroy();
+    }
+});
+
+/**
+ * This will modify the methods to convert the shorthand
+ * methods to the full methods.
+ *
+ * @param {object} component
+ * @returns {object}
+ */
+const modifyMethods = (component) =>
+{
+    const methods = Object.assign(getDefaultMethods(), component);
+    Object.assign(component.prototype, methods);
+
+    return component;
+};
+
+/**
+ * This will create a class.
+ *
+ * @param {object} Base
+ * @returns {object}
+ */
+const createClass = (Base) =>
+{
+    return class extends Base {}
+};
+
+/**
+ * This will create a pod component.
+ *
+ * @param {function} callBack
+ * @returns {object}
+ */
+export const Pod = (callBack) =>
+{
+    if (!callBack)
+    {
+        return null;
+    }
+
+    const component = createClass(Component);
+
+    /**
+     * This will call the callback function and pass the component
+     * to set up the component.
+     *
+     * The result is the render method.
+     */
+    const render = callBack(component);
+
+    /**
+     * This will modify the methods to convert the shorthand
+     * methods to the full methods.
+     */
+    modifyMethods(component);
+
+    component.prototype.render = render;
+    return component;
+};
 
 export const PodToDoApp = Pod((self) =>
 {
-	self.data = new Data({ items: [] });
-
-	self.created = () => {
-
+	self.created = () =>
+    {
+        console.log('created');
 	};
+
+    self.setData = () => new Data({ items: [] });
 
 	self.setStates = () => ({
 		loaded: true
@@ -15,7 +132,7 @@ export const PodToDoApp = Pod((self) =>
 
 	self.before = () =>
 	{
-		console.log('Mounted');
+		console.log('before');
 	};
 
 	self.after = () =>
@@ -25,35 +142,35 @@ export const PodToDoApp = Pod((self) =>
 
 	self.destroy = () =>
 	{
-		console.log('after');
+		console.log('destroy');
 	};
 
     // handle form submission
-    const handleSubmit = (event) =>
+    const handleSubmit = (event, { data }) =>
     {
         event.preventDefault();
         const form = event.target;
         const input = form.querySelector('input');
 
         // add the new to-do item to the array of items
-        self.data.push('items', input.value);
+        data.push('items', input.value);
         input.value = '';
     };
 
     // handle item removal
-    const handleRemove = (index) => self.data.splice('items', index);
+    const handleRemove = (index, data) => data.splice('items', index);
 
 	return () => Div([
-		H1('To-Do App'),
-		Form({ submit:handleSubmit }, [
-			Input({ placeholder: 'Add a new item' }),
-			Button({ type: 'submit' }, 'Add')
-		]),
-		Ul({
-			for: [self.data, 'items', (text, index) => Li({
-				text,
-				button: Button({ click: () => handleRemove(index) }, 'Remove')
-			})]
-		})
-	])
+        H1('To-Do App'),
+        Form({ submit: handleSubmit }, [
+            Input({ placeholder: 'Add a new item' }),
+            Button({ type: 'submit' }, 'Add')
+        ]),
+        Ul({
+            for: ['items', (text, index) => Li({
+                text,
+                button: Button({ click: (e, { data }) => handleRemove(index, data) }, 'Remove')
+            })]
+        })
+    ])
 });
