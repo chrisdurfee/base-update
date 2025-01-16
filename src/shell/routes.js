07563646e4025f51ffcaf09572a24a5c21d4552a
tech_e <chris.durfee@dentalqore.com>
2025-01-15 18:35:24 -0700

    Implement dynamic routing for album page; replace static import with lazy loading

--- src/shell/routes.js
+++ src/shell/routes.js
@@ -1,7 +1,6 @@
 import { CastPage } from '../components/pages/cast-page.js';
 import { HomePage } from '../components/pages/home/home-page.js';
 import { ListPage } from '../components/pages/list/list-page.js';
-import { AlbumPage } from '../components/pages/music/album-page/album-page.js';
 import { MusicPage } from '../components/pages/music/music-page/music-page.js';
 import { SynopsisPage } from '../components/pages/synopsis/synopsis-page.js';
 
@@ -24,13 +23,31 @@ const Route = (uri, component, title) =>
 };
 
 /**
+ * This will create a dynamic route object.
+ *
+ * @param {string} uri
+ * @param {function} callBack
+ * @param {string} title
+ * @returns {object}
+ */
+const DynamicRoute = (uri, callBack, title) =>
+{
+	return {
+		uri,
+		import: callBack,
+		title,
+		persist: false
+	};
+};
+
+/**
  * This will get the routes.
  *
  * @return {array}
  */
 export const Routes = () => [
 	Route('/', HomePage(), 'Example'),
-	Route('/music/album/:album?*', AlbumPage(), 'Example'),
+	DynamicRoute('/music/album/:album?*', () => import('../components/pages/music/album-page/album-page.js'), 'Album'),
 	Route('/music*', MusicPage(), 'Music'),
 	Route('/synopsis/:page?*', SynopsisPage(), 'Synopsis'),
 	Route('/cast', CastPage(), 'Cast'),
