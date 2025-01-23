92e3bacfd72652d1d09e29266d60eadc904c2b09
tech_e <chris.durfee@dentalqore.com>
2025-01-23 14:14:20 -0700

    Update Vite configuration to use base URL from configs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import { Configs } from './src/configs.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [],
-	base: '/base-update/',
+	base: Configs.router.baseUrl,
 	server: {
 		open: true
 	},
