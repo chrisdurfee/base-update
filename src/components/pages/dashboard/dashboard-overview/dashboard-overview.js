cdd4ccfaf8b1c0eb2a3a4c5247b42a3d3cc978c5
tech_e <chris.durfee@dentalqore.com>
2025-08-28 15:17:59 -0600

    feat: add new dashboard cards for reviews, sales by location, and top products
    - Implemented ReviewsCard component to display customer reviews with a breakdown of ratings.
    - Created SalesByLocationCard to show sales data by country with percentage changes.
    - Developed TopProductsCard featuring a horizontal bar chart of top-selling products using D3.js.
    - Removed deprecated DashboardCard and integrated its functionality into the new cards.
    - Updated OverviewDashboard to include new cards and reorganized layout for better UX.
    - Refactored VisitorsCard to enhance donut chart visualization and improve code readability.

--- src/components/pages/dashboard/dashboard-overview/dashboard-overview.js
+++ src/components/pages/dashboard/dashboard-overview/dashboard-overview.js
@@ -1,9 +1,32 @@
 import { Div, H1, Header } from "@base-framework/atoms";
 import { Button } from "@base-framework/ui/atoms";
-import { Icons } from "@base-framework/ui/icons";
-import { DatePicker } from "@base-framework/ui/molecules";
+import { DateRangePicker } from "@base-framework/ui/molecules";
 import { TabNavigation } from "@base-framework/ui/organisms";
 import { Page } from "@base-framework/ui/pages";
+
+/**
+ * Get the start or end date of the current month.
+ *
+ * @param {string} type - The type of date to get ('start' or 'end').
+ * @returns {string} - The start or end date of the current month.
+ */
+const getDate = (type = 'start') =>
+{
+	const date = new Date();
+	// Set the date to the start or end of the month
+	if (type === 'start')
+	{
+		date.setDate(1);
+		date.setHours(0, 0, 0, 0);
+	}
+	else
+	{
+		date.setMonth(date.getMonth() + 1);
+		date.setDate(0);
+	}
+
+	return date.toISOString().slice(0, 10);
+};
 
 /**
  * This will create the DashboardHeader molecule.
@@ -14,7 +37,10 @@ const DashboardHeader = () => (
 	Header({ class: 'flex items-center justify-between' }, [
 		H1({ class: 'text-3xl font-bold' }, 'Dashboard'),
 		Div({ class: 'flex items-center gap-2' }, [
-			new DatePicker({ label: 'Pick a date', icon: Icons.calendar }),
+			new DateRangePicker({
+				start: getDate('start'),
+				end: getDate('end')
+			}),
 			Div({ class: 'hidden md:flex items-center gap-2' }, [
 				Button({ variant: 'default' }, 'Download')
 			])
@@ -46,15 +72,15 @@ const DashboardTabs = () => (
  */
 export const DashboardOverview = () => (
 	new Page([
-		Div({ class: 'grid grid-cols-1 p-6 pt-0 sm:pt-6 lg:p-8 space-y-4' }, [
+		Div({ class: 'grid grid-cols-1 p-6 pt-0 sm:pt-6 lg:p-8 gap-y-4' }, [
 
 			DashboardHeader(),
 			DashboardTabs(),
 
 			Div({
 				switch: [
-					{ uri: 'dashboard', import: import('./dashboards/overview/overview-dashboard.js') },
-					{ uri: 'dashboard/overview/analytics', import: import('./dashboards/analytics/analytics-dashboard.js') }
+					{ uri: 'dashboard', import: () => import('./dashboards/overview/overview-dashboard.js') },
+					{ uri: 'dashboard/overview/analytics', import: () => import('./dashboards/analytics/analytics-dashboard.js') }
 				]
 			})
 		])
--- src/components/pages/dashboard/dashboard-overview/dashboards/overview/overview-dashboard.js
+++ src/components/pages/dashboard/dashboard-overview/dashboards/overview/overview-dashboard.js
@@ -1,9 +1,13 @@
 import { Div } from "@base-framework/atoms";
-import { Panel } from "@base-framework/ui/organisms";
+import { GradientLineChartCard } from "./cards/gradient-line-chart-card.js";
+import { KpiCardsRow } from "./cards/kpi-card.js";
+import { MetricsCard } from "./cards/metrics-card.js";
+import { OverviewCard } from "./cards/overview-card.js";
+import { RecentSalesCard } from "./cards/recent-sales-card.js";
+import ReviewsCard from "./cards/reviews-card.js";
+import SalesByLocationCard from "./cards/sales-by-location-card.js";
+import { TopProductsCard } from "./cards/top-products-card.js";
 import { DashboardSummaryCards } from "./dashboard-summary-cards.js";
-import { GradientLineChartCard } from "./gradient-line-chart-card.js";
-import { OverviewCard } from "./overview-card.js";
-import { RecentSalesCard } from "./recent-sales-card.js";
 import { VisitorsCard } from "./visitors-card.js";
 
 /**
@@ -12,20 +16,27 @@ import { VisitorsCard } from "./visitors-card.js";
  * @returns {object}
  */
 export const OverviewDashboard = () => (
-    new Panel([
-        Div({ class: 'mt-2 lg:mt-0 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 space-y-4' }, [
+	Div({ class: 'mt-2 lg:mt-0 flex flex-auto flex-col ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 gap-y-4' }, [
 
-            DashboardSummaryCards(),
-            Div({ class: 'grid md:gap-4 space-y-4 md:space-y-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-7' }, [
-                OverviewCard(),
-                RecentSalesCard(),
-            ]),
-            Div({ class: 'grid md:gap-4 space-y-4 md:space-y-0 grid-cols-1 md:grid-cols-4 lg:grid-cols-7' }, [
-                VisitorsCard(),
-                GradientLineChartCard(),
-            ])
-        ])
-    ])
+		DashboardSummaryCards(),
+		Div({ class: 'grid md:gap-4 gap-y-4 md:gap-y-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-7' }, [
+			OverviewCard(),
+			RecentSalesCard(),
+		]),
+		Div({ class: 'grid md:gap-4 gap-y-4 md:gap-y-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-7' }, [
+			MetricsCard(),
+			TopProductsCard(),
+		]),
+		KpiCardsRow(),
+		Div({ class: 'grid md:gap-4 gap-y-4 md:gap-y-0 grid-cols-1 md:grid-cols-4 lg:grid-cols-7' }, [
+			VisitorsCard(),
+			GradientLineChartCard(),
+		]),
+		Div({ class: 'grid md:gap-4 gap-y-4 md:gap-y-0 grid-cols-1 md:grid-cols-4 lg:grid-cols-7' }, [
+			SalesByLocationCard(),
+			ReviewsCard(),
+		])
+	])
 );
 
 export default OverviewDashboard;
