f5dc13e478b27774f1b3199d802f150df61bf1e6
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-08-28 21:21:18 +0000

    deploy: 0a4a66ce2d82c9704831d0546706cbc6955bcabb

--- assets/settings-section-B_MJf91r.js
+++
@@ -1,1 +0,0 @@
-import{n as e,b as a,f as i,r as l,a3 as c}from"./index-DZmvxLXW.js";const n=t=>i({class:"scroll-m-20 text-3xl font-bold tracking-tight"},t.title),m=e((t,s)=>a({class:`${t.class||""}`},[t.title&&n({title:t.title}),t.description&&l({class:"text-base text-muted-foreground py-2 max-w-[700px]"},t.description),c({class:"space-y-6 py-10",submit:t.submit},s)]));export{m as S};
